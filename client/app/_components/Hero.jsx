55997fb90fcbefd65b631a47a02fca473c7ff4e8
HasithGam <159763235+HasithGam@users.noreply.github.com>
2024-07-12 16:34:37 +1000

    WIP

---
+++ client/app/_components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+        <section className="bg-gray-50">
+            <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+                <div className="mx-auto max-w-xl text-center">
+                <h1 className="text-3xl font-extrabold sm:text-5xl">
+                    Manage Your Income
+                    <strong className="font-extrabold text-primary sm:block"> Control Your Expense </strong>
+                </h1>
+
+                <p className="mt-4 sm:text-xl/relaxed">
+                    Start creaing budget and secure your futue
+                </p>
+
+                <div className="mt-8 flex flex-wrap justify-center gap-4">
+                    <a
+                    className="block w-full rounded bg-orange-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-orange-600 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                    href="#"
+                    >
+                    Get Started
+                    </a>
+                </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Hero
