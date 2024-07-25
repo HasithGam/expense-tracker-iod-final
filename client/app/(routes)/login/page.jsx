f2439754bd95a9b08e84199d9825577dab8efc73
HasithGam <159763235+HasithGam@users.noreply.github.com>
2024-07-25 20:32:44 +1000

    WIP

--- client/app/(routes)/login/page.jsx
+++ client/app/(routes)/login/page.jsx
@@ -5,20 +5,43 @@ import useAuth from '@/lib/useAuth';
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const router = useRouter();
   const { login } = useAuth();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const router = useRouter();
+
+  const [loginError, setLoginError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  
 
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
+
+      let hasError = false;
+      setNameError("");
+      setEmailError("");
+      setPasswordError("");
+
+      if (email.trim() === "") {
+      setEmailError("Please enter a valid email address.");
+      hasError = true;
+      }
+
+      if (password.trim() === "") {
+        setPasswordError("Your password must contain between 4 and 60 characters.");
+        hasError = true;
+      }
+
+      if (hasError) return;
+
       await login(email, password);
       router.push('/dashboard');
     } catch (error) {
       console.error('Login failed:', error);
-      // error message
+      setPasswordError("Your username or password is incorrect!");
     }
   }
 
@@ -28,20 +51,27 @@ const LoginPage = () => {
         <div className='login-form'>
           <h1>Login</h1>
           <form onSubmit={handleLogin}>
-            <input
-              value={email}
-              className={`text-style`}
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              placeholder="Email"
-            />
-            <input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              placeholder="Password"
-            />
+            <>
+              <input
+                value={email}
+                className={`text-style`}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email"
+              />
+              {emailError && <p className="error-message">{emailError}</p>}
+            </>
+            <>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+              {emailError && <p className="error-message">{emailError}</p>}
+            </>
             <button type="submit">Login</button>
+            {emailError && <p className="error-message">{emailError}</p>}
             <div className="form-help">
               <div className="remember">
                 <input type="checkbox" id="remember" />
